14f5ec857126f3ba8f67c83754997e443ee52ecc
Hannah Boinowitz <hannah.boinowitz@web.de>
2022-02-21 17:33:12 +0100

    Lint tests

--- tests/test_dashboard_utils.py
+++ tests/test_dashboard_utils.py
@@ -7,12 +7,12 @@ class TestDashboardUtils:
     def teams(self):
         teams = pd.read_parquet("nfl_analysis/data/teams.parquet")
         return teams
-    
+
     @pytest.fixture
     def superbowls(self):
         superbowls = pd.read_parquet("nfl_analysis/data/superbowls.parquet")
         return superbowls
-    
+
     @pytest.fixture
     def mapping(self):
         mapping = pd.read_parquet("nfl_analysis/data/team_mapping.parquet")
@@ -21,29 +21,49 @@ class TestDashboardUtils:
     @pytest.fixture
     def num_of_superbowls(self, superbowls):
         from datetime import datetime
+
         num_of_superbowls = (
-            superbowls[superbowls['datum'] < datetime.today()].drop_duplicates('datum').reset_index().shape[0]
+            superbowls[superbowls["datum"] < datetime.today()]
+            .drop_duplicates("datum")
+            .reset_index()
+            .shape[0]
         )
         return num_of_superbowls
 
     def test_add_league(self, teams):
         teams = utils.add_league_for_teams(teams)
-        assert (teams.loc[16:, 'league'] == 'NFC').all()
-        assert (teams.loc[:16, 'league'] == 'AFC').all()
+        assert (teams.loc[16:, "league"] == "NFC").all()
+        assert (teams.loc[:16, "league"] == "AFC").all()
 
-    def test_expand_superbowl_data_vertically(self, superbowls, mapping, teams, num_of_superbowls):
-        expanded_data = utils.expand_superbowl_data_vertically(superbowls, mapping, teams)
-        expanded_data_agg = expanded_data.groupby(['league', 'division']).agg(wins = ('winner', 'sum'), losses = ('looser', 'sum'))
-        assert expanded_data_agg.sum().loc['wins'] == expanded_data_agg.sum().loc['losses']
-        assert expanded_data_agg.sum().loc['wins'] == num_of_superbowls
-    
-    def test_add_current_team_names_to_superbowl_data(self, superbowls, mapping, num_of_superbowls):
-        superbowls_current_teams = (
-            utils.add_current_team_names_to_superbowl_data(superbowls, mapping).drop_duplicates('datum')
+    def test_expand_superbowl_data_vertically(
+        self, superbowls, mapping, teams, num_of_superbowls
+    ):
+        expanded_data = utils.expand_superbowl_data_vertically(
+            superbowls, mapping, teams
         )
-        
+        expanded_data_agg = expanded_data.groupby(["league", "division"]).agg(
+            wins=("winner", "sum"), losses=("looser", "sum")
+        )
+        assert (
+            expanded_data_agg.sum().loc["wins"] == expanded_data_agg.sum().loc["losses"]
+        )
+        assert expanded_data_agg.sum().loc["wins"] == num_of_superbowls
+
+    def test_add_current_team_names_to_superbowl_data(
+        self, superbowls, mapping, num_of_superbowls
+    ):
+        superbowls_current_teams = utils.add_current_team_names_to_superbowl_data(
+            superbowls, mapping
+        ).drop_duplicates("datum")
+
         assert superbowls_current_teams.shape == (num_of_superbowls, 3)
-        assert superbowls_current_teams['current_name_looser'].isin(mapping['current_name']).all()
-        
-        expected_columns = ['datum', 'current_name_looser', 'current_name_winner']
-        assert all([column in expected_columns for column in superbowls_current_teams.columns])
+        assert (
+            superbowls_current_teams["current_name_looser"]
+            .isin(mapping["current_name"])
+            .all()
+        )
+
+        expected_columns = ["datum", "current_name_looser", "current_name_winner"]
+        assert all(
+            [column in expected_columns for column in superbowls_current_teams.columns]
+        )
--- tests/test_etl_utils.py
+++ tests/test_etl_utils.py
@@ -3,28 +3,29 @@ import pytest
 import pandas as pd
 from nfl_analysis.etl import utils
 
+
 class TestETLUtils:
     @pytest.fixture
     def teams(self):
         teams = pd.read_parquet("nfl_analysis/data/teams.parquet")
         return teams
-    
+
     @pytest.fixture
     def superbowls(self):
         superbowls = pd.read_parquet("nfl_analysis/data/superbowls.parquet")
         return superbowls
-    
+
     @pytest.fixture
     def mapping(self):
         mapping = pd.read_parquet("nfl_analysis/data/team_mapping.parquet")
         return mapping
 
     def test_coordinates_usa(self, teams):
-        range_latitude = [19.50139, 64.85694] 
+        range_latitude = [19.50139, 64.85694]
         range_longitude = [-161.75583, -68.01197]
-        assert range_latitude[0] <= teams['lat'].max() <= range_latitude[1]
-        assert range_longitude[0] <= teams['lon'].max() <= range_longitude[1]
-    
+        assert range_latitude[0] <= teams["lat"].max() <= range_latitude[1]
+        assert range_longitude[0] <= teams["lon"].max() <= range_longitude[1]
+
     def test_parse_coordinates(self):
         lat, lon = utils.parse_coordinates("39.278°N 76.623°W")
         assert lat == 39.278
@@ -32,18 +33,23 @@ class TestETLUtils:
 
         # Check missing Longitude
         with pytest.raises(ValueError):
-            utils.parse_coordinates('39.278°N')
-        
+            utils.parse_coordinates("39.278°N")
+
         # Check missing Latitude
         with pytest.raises(ValueError):
-            utils.parse_coordinates('76.623°W')
-    
+            utils.parse_coordinates("76.623°W")
+
     def test_parse_historic_team_names(self, mapping):
-        expected_rams_tuple = ('Los Angeles Rams', ['Los Angeles Rams', 'St. Louis Rams'])
-        expected_cardinals_tuple = ('Arizona Cardinals', ['Arizona Cardinals'])
-        assert expected_rams_tuple == utils.parse_historic_team_names("Los Angeles Rams(St. Louis Rams 1995–2015)")
-        assert expected_cardinals_tuple == utils.parse_historic_team_names("Arizona Cardinals")
+        expected_rams_tuple = (
+            "Los Angeles Rams",
+            ["Los Angeles Rams", "St. Louis Rams"],
+        )
+        expected_cardinals_tuple = ("Arizona Cardinals", ["Arizona Cardinals"])
+        assert expected_rams_tuple == utils.parse_historic_team_names(
+            "Los Angeles Rams(St. Louis Rams 1995–2015)"
+        )
+        assert expected_cardinals_tuple == utils.parse_historic_team_names(
+            "Arizona Cardinals"
+        )
 
-        assert mapping['current_name'].isin(mapping['historic_name']).all()
-
-
+        assert mapping["current_name"].isin(mapping["historic_name"]).all()
