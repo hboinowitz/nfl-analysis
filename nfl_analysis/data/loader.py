9c0f705966d23b3d96962b7bd12cd77b8453a14b
Hannah Boinowitz <hannah.boinowitz@web.de>
2022-02-22 21:45:19 +0100

    Add loader for parquet files

---
+++ nfl_analysis/data/loader.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+
+def load(table_name: str) -> pd.DataFrame:
+    path = os.path.dirname(os.path.realpath(__file__))
+    try:
+        table = pd.read_parquet(os.path.join(path, f"{table_name}.parquet"))
+        return table
+    except FileNotFoundError:
+        raise ValueError("The table with the given `table_name` does not exist.")
